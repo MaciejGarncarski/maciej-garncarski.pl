ac4a462277cf0b0a24efe7352e54c60ef29184ad
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-07-27 14:03:01 +0200

    feat: add new tech icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   experimental: {
+    clientPrerender: true,
     fonts: [
       {
         provider: fontProviders.google(),
