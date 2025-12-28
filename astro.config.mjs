ec8bb50ca6e2ece00374adf0062e9359f0fd2bcc
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-12-28 18:00:13 +0100

    feat: update anchor link styles and behavior

--- astro.config.mjs
+++ astro.config.mjs
@@ -81,14 +81,50 @@ export default defineConfig({
       [
         rehypeAutolinkHeadings,
         {
-          behavior: "append",
+          behavior: "wrap",
           content: {
-            children: [{ type: "text", value: "#" }],
             type: "element",
             tagName: "span",
             properties: {
               ["data-anchor-hash"]: ""
-            }
+            },
+            children: [
+              {
+                type: "element",
+                tagName: "svg",
+                properties: {
+                  xmlns: "http://www.w3.org/2000/svg",
+                  width: "18",
+                  height: "18",
+                  viewBox: "0 0 24 24",
+                  fill: "none",
+                  stroke: "currentColor",
+                  strokeWidth: "2",
+                  strokeLinecap: "round",
+                  strokeLinejoin: "round",
+                  className: ["lucide", "lucide-link-icon", "lucide-link"],
+                  "aria-hidden": "true"
+                },
+                children: [
+                  {
+                    type: "element",
+                    tagName: "path",
+                    properties: {
+                      d: "M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"
+                    },
+                    children: []
+                  },
+                  {
+                    type: "element",
+                    tagName: "path",
+                    properties: {
+                      d: "M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"
+                    },
+                    children: []
+                  }
+                ]
+              }
+            ]
           },
           headingProperties: {
             ["data-anchor"]: ""
