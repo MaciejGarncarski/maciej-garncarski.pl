2c0293a385212d440e053bca6f841f87fb380c43
Maciej Garncarski <maciejgarncarski@protonmail.com>
2026-03-18 18:12:32 +0100

    refactor: scroll indicator style, wpm

--- src/utils/calculate-read-time.ts
+++ src/utils/calculate-read-time.ts
@@ -1,5 +1,5 @@
 export function calculateReadingTime(mdxContent: string) {
-   const wordsPerMinute = 200;
+   const wordsPerMinute = 220;
 
    const text = mdxContent.replace(/<\/?[^>]+(>|$)/g, "");
    const wordCount = text.split(/\s+/).filter((word: any) => word.length > 0).length;
